be10bb2ffb9ff6d372b70078474a43ec5fdd1e5f
Tabea Reiffer <tr@cyon.ch>
2021-09-22 14:44:14 +0200

    wip

--- footer.php
+++ footer.php
@@ -4,7 +4,8 @@
     <nav>
 <ul class="list">
     <li><a href="/index.php"> Das ePortfolio</a></li>
-    <li class="dropdown"><a href="/journal/index.php"> Die Lerndokumentation</a><ul class="sublist">
+    <li class="dropdown"><a href="/journal/index.php"> Die Lerndokumentation</a>
+        <ul class="sublist">
             <li><a href="/journal/2021-08-20.php"> Kompetenzen</a></li>
             <li><a href="/journal/2021-08-27.php"> Iperka</a></li>
             <li><a href="/journal/2021-09-03.php"> Problemlösungskonzepte</a></li>
--- header.php
+++ header.php
@@ -6,4 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= isset($title) ? $title : "Das ePortfolio"?></title>
     <link href="/style.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Gemunu+Libre:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
 </head>
